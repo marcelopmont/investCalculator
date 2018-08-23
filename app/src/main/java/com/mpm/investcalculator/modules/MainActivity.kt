d8f26bb9c147dbc581ab03c417cb5bb6ac007c27
Marcelo Montanher <marcelomontanher@Marcelos-Mac-mini.local>
2018-08-22 21:33:14 -0300

    Fix request parameters and finalize first window

--- app/src/main/java/com/mpm/investcalculator/modules/MainActivity.kt
+++ app/src/main/java/com/mpm/investcalculator/modules/MainActivity.kt
@@ -1,28 +1,63 @@
 package com.mpm.investcalculator.modules
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import com.mpm.investcalculator.R
 import com.mpm.investcalculator.models.InvestmentRequestModel
 import com.mpm.investcalculator.models.InvestmentResponseModel
 import com.mpm.investcalculator.services.CalculatorService
 import com.mpm.investcalculator.utils.StringUtils
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View.OnFocusChangeListener
+import android.widget.DatePicker
+import kotlinx.android.synthetic.main.loading_view.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
+
+    companion object {
+        const val BUTTON_OPAQUE = 1f
+        const val BUTTON_TRANSLUCENT = 0.3f
+    }
 
     val stringUtils = StringUtils()
+
+    var selectedDay = 0
+    var selectedMonth = 0
+    var selectedYear = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupInitialDate()
         setupTextListeners()
         setupButton()
+    }
+
+    override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
+        selectedDay = day
+        selectedMonth = month + 1
+        selectedYear = year
+
+        homeDeadlineInput.setText(stringUtils.convertToBrazilianDateFormat(selectedDay, selectedMonth, selectedYear))
+
+        enableSimulateButton(isFieldsCompleted())
+    }
+
+    private fun setupInitialDate() {
+        val calendar = Calendar.getInstance()
+
+        selectedDay = calendar.get(Calendar.DAY_OF_MONTH)
+        selectedMonth = calendar.get(Calendar.MONTH) + 1
+        selectedYear = calendar.get(Calendar.YEAR)
     }
 
     private fun setupTextListeners() {
@@ -37,26 +72,21 @@ class MainActivity : AppCompatActivity() {
                     homeApplyLabelInput.setText(formattedValue)
                     homeApplyLabelInput.setSelection(formattedValue.length)
                     homeApplyLabelInput.addTextChangedListener(this)
+
+                    enableSimulateButton(isFieldsCompleted())
                 }
 
             }
         })
 
-        homeDeadlineInput.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {}
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                if (!p0.isNullOrEmpty()) {
-                    homeDeadlineInput.removeTextChangedListener(this)
-                    val formattedValue = stringUtils.convertToDateFormat(p0.toString())
-                    homeDeadlineInput.setText(formattedValue)
-                    homeDeadlineInput.setSelection(formattedValue.length)
-                    homeDeadlineInput.addTextChangedListener(this)
-                }
-
+        homeDeadlineInput.onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                val datePickerDialog = DatePickerDialog(this, this@MainActivity, selectedYear, selectedMonth - 1, selectedDay)
+                datePickerDialog.show()
             }
-        })
+            homePercentageInput.requestFocus()
+
+        }
 
         homePercentageInput.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {}
@@ -69,6 +99,8 @@ class MainActivity : AppCompatActivity() {
                     homePercentageInput.setText(formattedValue)
                     homePercentageInput.setSelection(formattedValue.length - 1)
                     homePercentageInput.addTextChangedListener(this)
+
+                    enableSimulateButton(isFieldsCompleted())
                 }
 
             }
@@ -86,19 +118,23 @@ class MainActivity : AppCompatActivity() {
     private fun onSimulateButtonClicked() {
         val calculatorService = CalculatorService()
 
+        loadingView.visibility = View.VISIBLE
+
         calculatorService.subscribeForCDI(getInvestmentRequestModel(),
                 { response ->
                     onCalculatorResponseSuccess(response)
+                    loadingView.visibility = View.GONE
                 },
                 {
                     onCalculatorResponseError()
+                    loadingView.visibility = View.GONE
                 })
     }
 
     private fun getInvestmentRequestModel(): InvestmentRequestModel {
-        val investedAmount = homeApplyLabelInput.text.toString().toDouble()
-        val rate = homePercentageInput.text.toString().toInt()
-        val maturityDate = homeDeadlineInput.text.toString()
+        val investedAmount = stringUtils.getNumbersFromString(homeApplyLabelInput.text.toString(), true)
+        val rate = stringUtils.getNumbersFromString(homePercentageInput.text.toString(), false).toInt()
+        val maturityDate = stringUtils.convertToRequestDateFormat(selectedDay, selectedMonth, selectedYear)
 
         return InvestmentRequestModel(investedAmount, "CDI", rate, false, maturityDate)
     }
@@ -107,6 +143,19 @@ class MainActivity : AppCompatActivity() {
         homeApplyLabelInput.setText("")
         homePercentageInput.setText("")
         homeDeadlineInput.setText("")
+    }
+
+    private fun isFieldsCompleted(): Boolean {
+        return homeApplyLabelInput.text.isNotEmpty() && homePercentageInput.text.isNotEmpty() && homeDeadlineInput.text.isNotEmpty()
+    }
+
+    private fun enableSimulateButton(isEnable: Boolean) {
+        simulateButton.isEnabled = isEnable
+        if (isEnable) {
+            simulateButton.alpha = BUTTON_OPAQUE
+        } else {
+            simulateButton.alpha = BUTTON_TRANSLUCENT
+        }
     }
 
     private fun onCalculatorResponseSuccess(investmentResponseModel: InvestmentResponseModel) {
--- app/src/main/java/com/mpm/investcalculator/utils/StringUtils.kt
+++ app/src/main/java/com/mpm/investcalculator/utils/StringUtils.kt
@@ -2,14 +2,12 @@ package com.mpm.investcalculator.utils
 
 class StringUtils {
 
-    fun convertToDateFormat(text: String): String {
-        var finalDate = getNumbersFromString(text, false).toInt().toString()
-        if (finalDate.length > 4) {
-            finalDate = finalDate.substring(0, 2) + "/" + finalDate.substring(2, 4) + "/" + finalDate.substring(4, finalDate.length)
-        } else if (finalDate.length > 2) {
-            finalDate = finalDate.substring(0, 2) + "/" + finalDate.substring(2, finalDate.length)
-        }
-        return finalDate
+    fun convertToBrazilianDateFormat(day: Int, month: Int, year: Int): String {
+        return day.toString() + "/" + month + "/" + year
+    }
+
+    fun convertToRequestDateFormat(day: Int, month: Int, year: Int): String {
+        return year.toString() + "-" + month + "-" + day
     }
 
     fun convertToMoneyFormat(text: String): String {
@@ -19,6 +17,8 @@ class StringUtils {
     fun convertToPercentage(text: String): String {
         return String.format("%d%%", getNumbersFromString(text, false).toInt())
     }
+
+
 
     fun getNumbersFromString(text: String, isMoney: Boolean): Double {
         val removeRegex = Regex("[^x0-9]")
